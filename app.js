d0844a8f52a6bb6355d17ea841cc9141ab64fe64
masumbillah360 <masumbillah6584@gmail.com>
2022-08-29 13:36:16 +0600

    added dynamic default data

--- app.js
+++ app.js
@@ -4,10 +4,41 @@ const loadData =(search)=>{
     // const url = ' www.themealdb.com/api/json/v1/1/categories.php'
     fetch(url).then(res=>res.json()).then(data=>showData(data));
 }
+const loadDefaultData =()=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=beef`;
+    console.log(url);
+    // const url = ' www.themealdb.com/api/json/v1/1/categories.php'
+    fetch(url).then(res=>res.json()).then(data=>showDefaultData(data));
+}
 
 const showData=(data)=>{
     const mealContainer = document.getElementById('container');
     mealContainer.innerHTML = ''
+    data.meals.forEach(element => {
+        const mealHolder = document.createElement('article');
+        mealHolder.classList.add('col-md-4')
+        const mealInstruction = element.strInstructions;
+        const mealInst = mealInstruction.slice(0,100);
+        mealHolder.innerHTML = `
+        <div class="card">
+            <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">${element.strMeal}</h5>
+                <p class="card-title d-flex justify-content-between"><span>${element.strCategory}</span> <span>${element.strArea}</span></p>
+                <p class="card-text">${mealInst}</p>
+                <div class = "d-flex justify-content-between align-items-center">
+                <a href="#" class="btn btn-danger fw-bold">Add To Cart</a>
+                <a href="#" class="btn btn-warning fw-bold">Buy Now</a>
+                </div>
+            </div>
+        </div>
+        `
+       console.log(element);
+       mealContainer.appendChild(mealHolder);
+    });
+}
+const showDefaultData=(data)=>{
+    const mealContainer = document.getElementById('container');
     data.meals.forEach(element => {
         const mealHolder = document.createElement('article');
         mealHolder.classList.add('col-md-4')
@@ -39,3 +70,4 @@ const searchFood = () => {
     console.log(sText);
     sField.value = ''    
 }
+loadDefaultData();
